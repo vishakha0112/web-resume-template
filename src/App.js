9a1655b00ed015324f86d409a14ec0522d83e86e
vishakha0112 <89861784+vishakha0112@users.noreply.github.com>
2021-12-09 01:03:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+import {Resume} from "./container/resume";
+
+export const App = () => {
+  return (
+    <Resume/>
+  )
+}
+
+export default App;
